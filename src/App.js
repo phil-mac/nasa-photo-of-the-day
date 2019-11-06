6cb2de275680487b039d9ca47a8f3e38276ee42a
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-06 13:44:12 -0800

    api call working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  const [photoState, setPhotosState] = useState('');
+
+  useEffect(() =>{
+    const fetchPhotos = () =>
+      fetch('https://api.nasa.gov/planetary/apod?api_key=H2cw3TrncbbEQ1QTS8rowEz3X0xnhhzqdQIWu15i&date=2012-03-14')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setPhotosState(data.url);
+        })
+        
+    fetchPhotos();
+  }, [])
+
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <img src={photoState} alt='nasa photo' />
     </div>
   );
 }
